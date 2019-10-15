5469ebec32a3ec8661039f4b9e3f38ceb6e74f13
Edson Jr <eds_jr@hotmail.com>
2019-10-14 21:21:53 -0400

    [CollectionViewCell] - Criado o arquivo  de celula da colectionview e feito o bind dos elementos.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

---
+++ VitrineProdutos/View/Cells/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  VitrineProdutos
+//
+//  Created by Edson  Jr on 14/10/2019.
+//  Copyright © 2019 Edson  Jr. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
