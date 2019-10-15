7def857d054f3212895aba862c7f3677ad09c2d4
Edson Jr <eds_jr@hotmail.com>
2019-10-14 21:48:40 -0400

    [link.txt] - Criado um arquivo para conter os links dos banners. Os banners irao ser carregados a priori nos assets do projeto, no entanto, posteriormente serao carregados via web.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- VitrineProdutos/Controller/ViewController.swift
+++ VitrineProdutos/Controller/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
