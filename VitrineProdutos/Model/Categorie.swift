a874120d5df5727c16baba737821c5c749f1274b
Edson Jr <eds_jr@hotmail.com>
2019-11-11 19:15:06 -0400

    [Project] - Criada uma nova branch nomeada de EdsonJr_firestoreDev que contera a nova versao do aplicativo, porem agora com firestore database, ao inves do realtime database (firebase)
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

---
+++ VitrineProdutos/Model/Categorie.swift
@@ -0,0 +1,32 @@
+//
+//  Categoria.swift
+//  VitrineProdutos
+//
+//  Created by Edson  Jr on 30/10/2019.
+//  Copyright © 2019 Edson  Jr. All rights reserved.
+//
+
+import Foundation
+
+class Categorie{
+    
+    var name: String?
+    var shows: [Show]?
+    
+
+    //default init - sem parametros
+    init() {}
+    
+    
+    //init com parametros
+    init(name: String?,shows:[Show]?){
+        self.name = name
+        self.shows = shows
+    }
+    
+    
+    
+}
+
+
+
---
+++ VitrineProdutos/Model/Show.swift
@@ -0,0 +1,33 @@
+//
+//  Show.swift
+//  VitrineProdutos
+//
+//  Created by Edson  Jr on 11/11/2019.
+//  Copyright © 2019 Edson  Jr. All rights reserved.
+//
+
+import Foundation
+
+
+class Show {
+    
+    var id: String?
+    var imageUrl: String?
+    var startDate: Date?
+    var endDate: Date?
+    var showHouse: ShowHouse?
+    
+    
+    //default init
+    init() {}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
