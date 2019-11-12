6e367e4bb6198e895500064052c110fe42a85571
Edson Jr <eds_jr@hotmail.com>
2019-11-11 21:54:37 -0400

    [FirebaseDAO] - Resolvido o problema de crash que estava dando pois o iOS nao era capaz de configurar a conexao com o firestore.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- VitrineProdutos/Controller/ViewController.swift
+++ VitrineProdutos/Controller/ViewController.swift
@@ -33,9 +33,9 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         
         
 //        //Acessando o banco de dados via web e extraindo os dados
-//        self.DAO.retrieveData(collectionName: "categories") { (listOfCategories) in
-//            print("SOMENTE TESTANDO....")
-//        }
+        self.DAO.retrieveData(collectionName: "categories") { (listOfCategories) in
+            print("SOMENTE TESTANDO....")
+        }
         
     }
 
--- VitrineProdutos/Services/DAO/FirebaseDAO.swift
+++ VitrineProdutos/Services/DAO/FirebaseDAO.swift
@@ -16,14 +16,14 @@ class FirebaseDAO {
     
     let TAG = "[FirebaseDAO]: "
     //let rootRef = "categories"
-    let firestoreDB = Firestore.firestore()
+    lazy var firestoreDB = Firestore.firestore()
     
     
     //Retrieve data from Firestore
     func retrieveData(collectionName: String!,completionHandler: @escaping ([Categorie?])->()){
         
         var listOfData: Array<Categorie>? = []
-        self.firestoreDB.collection("tasks").getDocuments() { (querySnapshot, error) in
+        self.firestoreDB.collection(collectionName).getDocuments() { (querySnapshot, error) in
             if let error = error {
                 print(self.TAG + "Error getting documents: \(error)")
                 DispatchQueue.main.async {
